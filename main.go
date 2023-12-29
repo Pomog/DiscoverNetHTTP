6e2c78ac4300f54339aa2e58a995d06552f5d5f6
Pomog <thoryur@gmail.com>
2023-12-29 21:22:08 +0200

    login

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/Pomog/DiscoverNetHTTP/request"
 )
@@ -21,7 +22,7 @@ func main() {
 	client := http.Client{}
 
 	// URL for the GET request (http://localhost:8080 in this case)
-	urlAddress := "http://52.91.213.117/login" // http://52.91.213.117/login test server deployed on AWS
+	urlAddress := "http://localhost:8080/login" // http://52.91.213.117/login test server deployed on AWS
 
 	// Create an instance of HTTPRequestParams for the GET request
 	params := request.HTTPRequestParams{
@@ -33,7 +34,7 @@ func main() {
 	}
 
 	// Make a GET request to server
-	response, err := request.MakeRequest(params)
+	response, err := request.MakeGetRequest(params)
 	if err != nil {
 		log.Fatal("Error:", err)
 	}
@@ -45,11 +46,96 @@ func main() {
 	fmt.Println("Header:", response.Header)
 	fmt.Println("Cookies: ", response.Cookies())
 	fmt.Println("Request.UR:", response.Request.URL)
-	fmt.Println("response.TLS: ", response.TLS)
 
 	// Read and print the response body
 	body := make([]byte, 512)
 	_, _ = response.Body.Read(body)
 	fmt.Println("Response Body:", string(body))
 
+	// Create an instance of HTTPRequestParams for the GET request
+	paramsPOST := request.HTTPRequestParams{
+		Client:     &client,
+		URL:        urlAddress,
+		Method:     "POST",
+		TimeoutSec: 10,
+	}
+	// Add form data to the request
+	paramsPOST.Values = url.Values{}
+	paramsPOST.Values.Set("emailLogIn", "beavis@mtv.com")
+	paramsPOST.Values.Set("passwordLogIn", "123456")
+
+	// Make a Post request to server
+	response, err = request.MakePostRequest(paramsPOST)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	defer response.Body.Close()
+
+	// Process the response
+	fmt.Println("response to POST request")
+	fmt.Println("Status Code: ", response.Status)
+	fmt.Println("Header:", response.Header)
+	fmt.Println("Cookies: ", response.Cookies())
+	fmt.Println("Request.UR: ", response.Request.URL)
+
+	// Read and print the response body
+	POSTbody := make([]byte, 5000)
+	_, _ = response.Body.Read(POSTbody)
+	fmt.Println("Response Body:\n", string(POSTbody))
 }
+
+// // Create an instance of HTTPRequestParams for the GET request
+// paramsPOST := request.HTTPRequestParams{
+// 	Client:     &client,
+// 	URL:        urlAddress,
+// 	Method:     "POST",
+// 	TimeoutSec: 10,
+// }
+// // Add form data to the request
+// paramsPOST.Values = url.Values{}
+// paramsPOST.Values.Set("firstName", "John")
+// paramsPOST.Values.Set("lastName", "Doe")
+// paramsPOST.Values.Set("nickName", "JohnD")
+// paramsPOST.Values.Set("emailRegistr", "john.doe@example.com")
+// paramsPOST.Values.Set("passwordReg", "securePassword123")
+
+// // Open the file that you want to include in the request
+// file, err := os.Open("johvi.png")
+// if err != nil {
+// 	fmt.Println("Error opening file:", err)
+// 	return
+// }
+// defer file.Close()
+
+// // Create a new buffer to store the file contents
+// fileBuffer := &bytes.Buffer{}
+// if _, err := io.Copy(fileBuffer, file); err != nil {
+// 	fmt.Println("Error reading file:", err)
+// 	return
+// }
+
+// // Add the file to the request
+// paramsPOST.Files = map[string]io.Reader{
+// 	"avatar": fileBuffer,
+// }
+
+// // Make a Post request to server
+// response, err = request.MakePostRequest(paramsPOST)
+// if err != nil {
+// 	log.Fatal("Error:", err)
+// }
+// defer response.Body.Close()
+
+// // Process the response
+// fmt.Println("response to POST request")
+// fmt.Println("Status Code: ", response.Status)
+// fmt.Println("Header:", response.Header)
+// fmt.Println("Cookies: ", response.Cookies())
+// fmt.Println("Request.UR: ", response.Request.URL)
+// location, _ := response.Location()
+// fmt.Println("location: ", location)
+
+// // Read and print the response body
+// POSTbody := make([]byte, 5000)
+// _, _ = response.Body.Read(POSTbody)
+// fmt.Println("Response Body:\n", string(POSTbody))
--- request/request.go
+++ request/request.go
@@ -3,6 +3,7 @@ package request
 import (
 	"context"
 	"crypto/tls"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -11,20 +12,21 @@ import (
 // HTTPRequestParams represents the parameters for an HTTP request.
 type HTTPRequestParams struct {
 	Client         *http.Client
-	URL            string            // URL of the request
-	Method         string            // HTTP method (GET, POST, etc.)
-	Headers        map[string]string // HTTP headers
-	Cookies        []*http.Cookie    // Cookies
-	Values         url.Values        // Form values (for POST requests, etc.)
-	Body           string            // Request body for non-form data
-	TimeoutSec     int               // Timeout in seconds
-	UserAgent      string            // User-Agent header for the request
-	QueryParams    url.Values        // Additional query parameters
-	Username       string            // Username for authentication
-	Password       string            // Password for authentication
-	RequestContext context.Context   // Context for the request
-	ProxyURL       *url.URL          // URL of the proxy to be used for the request
-	TLSConfig      *tls.Config       // TLS configuration for the request
+	URL            string               // URL of the request
+	Method         string               // HTTP method (GET, POST, etc.)
+	Headers        map[string]string    // HTTP headers
+	Cookies        []*http.Cookie       // Cookies
+	Values         url.Values           // Form values (for POST requests, etc.)
+	Body           string               // Request body for non-form data
+	TimeoutSec     int                  // Timeout in seconds
+	UserAgent      string               // User-Agent header for the request
+	QueryParams    url.Values           // Additional query parameters
+	Username       string               // Username for authentication
+	Password       string               // Password for authentication
+	RequestContext context.Context      // Context for the request
+	ProxyURL       *url.URL             // URL of the proxy to be used for the request
+	TLSConfig      *tls.Config          // TLS configuration for the request
+	Files          map[string]io.Reader // Files to be included in the request
 	// Add more parameters as needed
 }
 
@@ -62,8 +64,52 @@ func generateGETrequest(params HTTPRequestParams) (*http.Request, error) {
 	return req, nil
 }
 
-func MakeRequest(params HTTPRequestParams) (*http.Response, error) {
+func MakeGetRequest(params HTTPRequestParams) (*http.Response, error) {
 	req, err := generateGETrequest(params)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set a timeout if specified
+	if params.TimeoutSec > 0 {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(params.TimeoutSec)*time.Second)
+		defer cancel()
+		req = req.WithContext(ctx)
+	}
+
+	return params.Client.Do(req)
+}
+
+func generatePOSTRequest(params HTTPRequestParams) (*http.Request, error) {
+	// Create a new HTTP request
+	req, err := http.NewRequest(params.Method, params.URL, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set request headers
+	for key, value := range params.Headers {
+		req.Header.Set(key, value)
+	}
+
+	// Set form values for POST requests
+	if params.Method == "POST" {
+		req.PostForm = params.Values
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	// Set the context for the request
+	if params.RequestContext != nil {
+		req = req.WithContext(params.RequestContext)
+	} else {
+		req = req.WithContext(context.Background())
+	}
+
+	return req, nil
+}
+
+func MakePostRequest(params HTTPRequestParams) (*http.Response, error) {
+	req, err := generatePOSTRequest(params)
 	if err != nil {
 		return nil, err
 	}
