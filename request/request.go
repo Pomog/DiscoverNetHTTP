effa6758f8fe5e7afd1234102a2908a80ccff14a
Pomog <thoryur@gmail.com>
2024-01-01 02:36:47 +0200

    PrintRequestParams

--- request/request.go
+++ request/request.go
@@ -149,11 +149,52 @@ func GeneratePOSTRequestWithFile(params HTTPRequestParams) (*http.Request, error
 	return req, nil
 }
 
+func PrintRequestParams(req *http.Request, params HTTPRequestParams) {
+	fmt.Printf("****************** Print the request details ******************\n")
+	fmt.Printf("Request Method: %s\n", req.Method)
+	fmt.Printf("Request URL: %s\n", req.URL)
+	fmt.Printf("Request Headers:\n")
+	for key, values := range req.Header {
+		for _, value := range values {
+			fmt.Printf("  %s: %s\n", key, value)
+		}
+	}
+	fmt.Printf("Request Cookies:\n")
+	for _, cookie := range req.Cookies() {
+		fmt.Printf("  %s: %s\n", cookie.Name, cookie.Value)
+	}
+	fmt.Printf("Request Values From HTTPRequestParams:\n")
+	for key, values := range params.Values {
+		for _, value := range values {
+			fmt.Printf("  %s: %s\n", key, value)
+		}
+	}
+	fmt.Printf("Request Timeout (seconds): %d\n", params.TimeoutSec)
+	fmt.Printf("Request UserAgent: %s\n", params.UserAgent)
+	fmt.Printf("Request Query Params:\n")
+	for key, values := range params.QueryParams {
+		for _, value := range values {
+			fmt.Printf("  %s: %s\n", key, value)
+		}
+	}
+	fmt.Printf("Request Username: %s\n", params.Username)
+	fmt.Printf("Request Password: %s\n", params.Password)
+	fmt.Printf("Request TLS Config: %v\n", params.TLSConfig)
+	fmt.Printf("Request Files:\n")
+	for key, _ := range params.Files {
+		fmt.Printf("	%s: \n", key)
+
+	}
+	fmt.Printf("***************************************************************\n")
+}
+
 func MakePostRequest(params HTTPRequestParams) (*http.Response, error) {
 	req, err := GeneratePOSTRequestWithFile(params)
 	if err != nil {
 		return nil, err
 	}
+
+	PrintRequestParams(req, params)
 
 	// Set a timeout if specified
 	if params.TimeoutSec > 0 {
